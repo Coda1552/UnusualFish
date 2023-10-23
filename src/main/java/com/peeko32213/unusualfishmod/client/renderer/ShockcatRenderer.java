101ff2eb3429a54cd2a13b4f51b562fe66bb5a0e
Coda <cody.koelliker@outlook.com>
2023-10-23 17:27:02 -0600

    more fixes

--- src/main/java/com/peeko32213/unusualfishmod/client/renderer/ShockcatRenderer.java
+++ src/main/java/com/peeko32213/unusualfishmod/client/renderer/ShockcatRenderer.java
@@ -24,7 +24,6 @@ public class ShockcatRenderer extends MobRenderer<Shockcat, ShockcatModel<Shockc
         return TEXTURE;
     }
 
-
     protected void setupRotations(Shockcat p_116226_, PoseStack p_116227_, float p_116228_, float p_116229_, float p_116230_) {
         super.setupRotations(p_116226_, p_116227_, p_116228_, p_116229_, p_116230_);
         float f = 4.3F * Mth.sin(0.5F * p_116228_);
