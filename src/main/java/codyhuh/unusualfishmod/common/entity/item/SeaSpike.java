9f74ddf33ed87c1b380f78278696a0bbf46890c5
Chakyl <chakyl1127@gmail.com>
2026-06-17 18:10:49 -0400

    seaspike saving error

--- src/main/java/codyhuh/unusualfishmod/common/entity/item/SeaSpike.java
+++ src/main/java/codyhuh/unusualfishmod/common/entity/item/SeaSpike.java
@@ -6,6 +6,7 @@ import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
@@ -46,12 +47,12 @@ public class SeaSpike extends AbstractArrow {
 
     @Override
     protected ItemStack getPickupItem() {
-        return ItemStack.EMPTY;
+        return new ItemStack(Items.ARROW);
     }
 
     @Override
     protected ItemStack getDefaultPickupItem() {
-        return ItemStack.EMPTY;
+        return new ItemStack(Items.ARROW);
     }
 
     @Override
